c527a250559df98bfd8e3cbcae1de0af71329462
MaxRStevens-1 <steve3mr@dukes.jmu.edu>
2022-04-04 08:58:26 -0400

    final (probably) update, alls left to do is write cool functions

--- main.js
+++ main.js
@@ -1,127 +1,147 @@
-import {VertexAttributes} from './vertex-attributes';
-import {ShaderProgram} from './shader-program';
-import {VertexArray} from './vertex-array';
-import {Vector3} from './vector';
+  import {VertexAttributes} from './vertex-attributes';
+  import {ShaderProgram} from './shader-program';
+  import {VertexArray} from './vertex-array';
+  import {Vector3} from './vector';
 
-let canvas;
-let attributes;
-let shaderProgram;
-let vao;
-let nInput;
-let radiusInput;
-let userInput;
-let testVar;
+  let canvas;
+  let attributes;
+  let shaderProgram;
+  let vao;
+  let nInput;
+  let radiusInput;
+  let userInput;
+  let mousePosX;
+  let mousePosY;
+  let pageStartTime;
 
-function render() {
-  gl.viewport(0, 0, canvas.width, canvas.height);
-  //gl.clearColor(1, 0.5, 0, 1);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  shaderProgram.bind();
-  //shaderProgram.setUniform3f('color', 1, 0, 0);
-  vao.bind();
-  vao.drawIndexed(gl.TRIANGLES);
-  vao.unbind();
-  shaderProgram.unbind();
-}
-
-function onResizeWindow() {
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
-  render();
-}
-async function initialize() {
-  canvas = document.getElementById('canvas');
-  window.gl = canvas.getContext('webgl2');
+  function render() {
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    //gl.clearColor(1, 0.5, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    shaderProgram.bind();
+    shaderProgram.setUniform2f ('dimensions', canvas.clientWidth, canvas.clientHeight);
+    shaderProgram.setUniform3f ('mouse', mousePosX, mousePosY, 0);
+    shaderProgram.setUniform3f ('colors', 0.0, 1.0, 1.0);
+    shaderProgram.setUniform1f ('time', getTime());
+    vao.bind();
+    vao.drawIndexed(gl.TRIANGLES);
+    vao.unbind();
+    shaderProgram.unbind();
+  }
 
 
-  const positions = [
-    -1,  1, 0,
-     1, -1, 0,
-    -1, -1, 0,
-     //0.7,  0.5, 0.0,
-     1, 1, 0
-  ];
+  // get time function/math found online
+  // https://stackoverflow.com/questions/41632942/how-to-measure-time-elapsed-on-javascript
+  function getTime () {
+    var endTime = new Date();
+    var timeDiff = endTime - pageStartTime; //in ms
+    // turn into seconds
+    timeDiff /= 1000;
+    // return unrounded seconds
+    return timeDiff;
+  }
 
-  const indices = [
-    2, 0, 1,
-    3, 1, 0
-  ];
+  function onResizeWindow() {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    render();
+  }
+  async function initialize() {
+    canvas = document.getElementById('canvas');
+    window.gl = canvas.getContext('webgl2');
+    pageStartTime = new Date();
 
-  const colors = [
-    1, 0, 0,
-    0, 1, 0,
-    0, 1, 1,
-    1, 0, 1
-  ];
+    // pretty sure this is depcriated 
+    const positions = [
+      -1,  1, 0,
+       1, -1, 0,
+      -1, -1, 0,
+       1,  1, 0
+    ];
 
-  userInput = 'vec3 color = vec3(1,0,1);'
-  
-  // VAO, VBO, shader program setup...
+    const indices = [
+      2, 0, 1,
+      3, 1, 0
+    ];
 
-  const attributes = new VertexAttributes();
-  attributes.addAttribute('position', 4, 3, positions);
-  attributes.addAttribute('color', 4, 3, colors);
-  attributes.addIndices(indices);
-  shaderProgramInit(attributes);
+    const colors = [
+      1, 0, 0,
+      0, 1, 0,
+      0, 1, 1,
+      1, 0, 1
+    ];
+
+    userInput = 'vec3 color = vec3(1,0,1);'
+    
+    // VAO, VBO, shader program setup...
+
+    const attributes = new VertexAttributes();
+    attributes.addAttribute('position', 4, 3, positions);
+    attributes.addIndices(indices);
+    shaderProgramHelper(attributes);
 
 
 
 
-  //generateCircle(50, 1);
 
-  // Event listeners
-  window.addEventListener('resize', onResizeWindow);
-  window.addEventListener('keydown', enterF);
+    // Event listeners
+    window.addEventListener('resize', onResizeWindow);
+    window.addEventListener('keydown', pressEnter);
+    window.addEventListener('mousemove', mouseTracker)
 
-    // other event listeners...
-    //nInput.addEventListener('input', synchronize);
-    //radiusInput.addEventListener('input', synchronize);
-  onResizeWindow();
-}
+    onResizeWindow();
+  }
 
-function enterF(event) {
-  if (event.key === 'Enter') {
-    var string = document.getElementById('input').value
-    userInput = string
-    console.log (string)
-    shaderProgram = shaderProgramInit(vao.attributes);
-    // DO SOMETHING
-
+  function mouseTracker (event) {
+    mousePosX = event.clientX
+    mousePosY = event.clientY
     render()
-    //console.log (testVar)
-  }
-}
-
-function shaderProgramInit (attributes) {
-  const vertexSource = `
-  in vec3 position;
-  out vec3 mixColor;
-  
-  
-  void main() {
-    gl_Position = vec4(position, 1.0);
-    mixColor = vec3 (0,1,1);
-  }
-  `;
-
-  const fragmentSource = `
-  in vec3 mixColor;
-  out vec4 fragmentColor;
-  
-  vec3 f() {
-    //vec3 color = vec3(1,1,1);
-    ${userInput}
-    return color; 
   }
 
-  void main() {
-    fragmentColor = vec4(f(), 1.0);
-  } 
-  `;
-  shaderProgram = new ShaderProgram(vertexSource, fragmentSource);
-  vao = new VertexArray(shaderProgram, attributes);
-  return shaderProgram;
-}
+  function pressEnter(event) {
+    if (event.key === 'Enter') {
+      userInput = document.getElementById('input').value
+      console.log (userInput)
+      shaderProgram = shaderProgramHelper(vao.attributes);
+      // DO SOMETHING
+      render()
+    }
+  }
 
-window.addEventListener('load', initialize);
+  function shaderProgramHelper (attributes) {
+    const vertexSource = `
+    in vec3 position;
+    out vec3 mixColor;
+    
+    
+    void main() {
+      gl_Position = vec4(position, 1.0);
+      mixColor = vec3 (0,1,1);
+    }
+    `;
+
+    const fragmentSource = `
+
+
+    uniform vec2 dimensions;
+    uniform vec3 mouse;
+    uniform float time;
+    uniform vec3 colors;
+    in vec3 mixColor;
+    out vec4 fragmentColor;
+    
+    vec3 f() {
+      ${userInput}
+      return color; 
+    }
+
+    void main() {
+      fragmentColor = vec4(f(), 1.0);
+    } 
+    `;
+    shaderProgram = new ShaderProgram(vertexSource, fragmentSource);
+    vao = new VertexArray(shaderProgram, attributes);
+    return shaderProgram; 
+  }
+
+  window.addEventListener('load', initialize);
